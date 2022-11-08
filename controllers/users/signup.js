6bb9feada1af1820dc01694493c6c3392dcaac58
Anastasiia Kazymko <92369437+AnastasiiaKazymko@users.noreply.github.com>
2022-11-08 16:28:12 +0200

    add verify

--- controllers/users/signup.js
+++ controllers/users/signup.js
@@ -1,8 +1,11 @@
 const bcrypt = require("bcrypt");
 const gravatar = require("gravatar");
+const { nanoid } = require("nanoid");
 
 const { User } = require("../../models/userSchema");
-const { RequestError } = require("../../helpers");
+const { RequestError, sendEmail } = require("../../helpers");
+
+const { BACE_URL } = process.env;
 
 const signup = async (req, res) => {
   const { email, password, subscription } = req.body;
@@ -12,12 +15,23 @@ const signup = async (req, res) => {
   }
   const hashPassword = await bcrypt.hash(password, 10);
   const avatarURL = gravatar.url(email);
+  const verificationToken = nanoid();
   const result = await User.create({
     email,
     password: hashPassword,
     subscription,
     avatarURL,
+    verificationToken,
   });
+
+  const mail = {
+    to: email,
+    subject: "Verify email",
+    html: `<a target="_blank" href="${BACE_URL}/api/signup/verify/${verificationToken}">Click to verify you email</a>`,
+  };
+
+  await sendEmail(mail);
+
   res.status(201).json({
     email: result.email,
     subscription: result.subscription,
